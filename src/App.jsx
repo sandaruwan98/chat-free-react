d444d3b571ed16e041e0b0cd3cc4b4b7099a47f3
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-10-29 21:50:53 +0530

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import ChatRoom from './components/ChatRoom'
+import SignIn from './components/SignIn'
+import SignOut from './components/SignOut'
 import  firebase from "firebase";
-import 'firebase/firestore'
 import 'firebase/auth'
-
 import { useAuthState } from "react-firebase-hooks/auth";
-
-// const auth = firebase.auth();
-// const firestore = firebase.firestore();
-
 
 function App() {
   const [user] = useAuthState(firebase.auth())
@@ -25,28 +21,6 @@ function App() {
     </div>
   );
 }
-
-function SignIn() {
-  const signInWithGoogle = () =>{
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
-  return (
-    <button onClick={signInWithGoogle} >Sign In </button>
-  )
-}
-
-function SignOut() {
- 
-  return firebase.auth().currentUser && (
-    <button onClick={()=> firebase.auth().signOut()} >Sign Out </button>
-  )
-}
-
-
-
-
-
 
 
 export default App;
